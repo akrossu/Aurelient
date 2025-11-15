a84439f28a53306799a74131ff3d5fb6c40e4973
Akross <59298840+akrossu@users.noreply.github.com>
2025-11-15 08:52:17 -0500

    initial commit

---
+++ frontend/src/components/icons/Send.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Send: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M9.912 12H4L2.023 4.135A.662.662 0 0 1 2 3.995c-.022-.721.772-1.221 1.46-.891L22 12L3.46 20.896c-.68.327-1.464-.159-1.46-.867a.66.66 0 0 1 .033-.186L3.5 15"
+    />
+  </svg>
+);
