0616fd9d9e8facad8c13fcf47fba1a1d795e7844
Ethan <epierce@solutionstreet.com>
2025-11-15 19:42:08 -0500

    migrate to use dotenv for most settings

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
+import dotenv from "dotenv"
+dotenv.config()
+
 import app from './app.js';
 import { PORT, USE_FAKE } from './src/config/settings.js';
 
 app.listen(PORT, () => {
   console.log(`backend http://localhost:${PORT}`);
+  console.log(`model: ${process.env.LMSTUDIO_MODEL}`)
   console.log(`mode: ${USE_FAKE ? "FAKE" : "REAL"}`);
 });
--- backend/src/config/settings.js
+++ backend/src/config/settings.js
@@ -1,6 +1,6 @@
 export const USE_FAKE = false;
-export const PORT = 4000;
+export const PORT = Number(process.env.APP_PORT) || 4000;
+export const LMSTUDIO_PORT = Number(process.env.LMSTUDIO_PORT) || 1234;
 
-export const LMSTUDIO_URL = "http://localhost:1234/v1/chat/completions";
-export const LMSTUDIO_MODEL =
-  process.env.LMSTUDIO_MODEL || "local-model";
+export const LMSTUDIO_URL = `http://localhost:${LMSTUDIO_PORT}/v1/chat/completions`;
+export const LMSTUDIO_MODEL = process.env.LMSTUDIO_MODEL || "local-model";
